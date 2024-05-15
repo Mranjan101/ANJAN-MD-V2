2389431ee191f89dffd55722afdb0a7077e8dbf0
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-16 03:21:53 +0530

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -14,7 +14,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `*≡ ʟɪɴᴋ ᴅᴇᴛᴇᴄᴛᴇᴅ ʙʏ ʀᴏᴍᴇᴋ-xᴅ-ʙᴏᴛ*
+        await conn.reply(m.chat, `*≡ ʟɪɴᴋ ᴅᴇᴛᴇᴄᴛᴇᴅ ʙʏ 𝞓𝞜𝙅𝞓𝞜-𝞛𝘿-𝝯𝟮*
             
 We do not allow links from other groups 
 I'm sorry *@${m.sender.split('@')[0]}*  you will be kicked out of the group ${isBotAdmin ? '' : '\n\nIM not an admin so I canT expel you :"v'}`, null, { mentions: [m.sender] } )
