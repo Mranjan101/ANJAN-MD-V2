6190682ddc7dc90ba6c542d55815f0f285beead1
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-16 17:42:45 +0530

    Update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -47,7 +47,7 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
      
 };
 
-handler.help = ['ytmp4 <yt-link>'];
+handler.help = ['ytv <yt-link>'];
 handler.tags = ['downloader'];
 handler.command = ['ytmp4', 'video', 'ytv'];
 handler.diamond = false;
