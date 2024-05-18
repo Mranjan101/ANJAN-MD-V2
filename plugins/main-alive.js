feee92671457d265472f5193aa64358a833c6097
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-18 17:24:47 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./Assets/ALIVE.mp3"
-    let url = "https://github.com/Xcelsama"
+    let url = "https://github.com/OfficialAnjanv2"
     let murl = "https://YouTube.com"
     let img = "https://i.imgur.com/emWHZGh.jpeg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '919341378016@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
