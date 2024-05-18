2c33d5257d36f685ee9e59816b120f811322461e
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-18 14:05:38 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
     let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
-let info = `*STAR BOT IS ACTIVE*`
+let info = `*ANJAN-MD-V2 BOT IS ACTIVE*`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
       isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}})
 }
