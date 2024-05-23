500cdc1ee9b1b9c11d31da3414833a48d8c46488
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-23 16:34:18 +0530

    Update GURU-Openai.js

--- plugins/GURU-Openai.js
+++ plugins/GURU-Openai.js
@@ -13,7 +13,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
     m.react(rwait)
     const { key } = await conn.sendMessage(m.chat, {
       image: { url: 'https://i.imgur.com/QLpzYtg.jpeg' },
-      caption: 'ᴿᴼᴹᴱᴷ-ˣᴰ-ᴮᴼᵀ ɪs ᴛʜɪɴᴋɪɴɢ....'
+      caption: 'ᴮᴼᵀ ɪs ᴛʜɪɴᴋɪɴɢ....'
     }, {quoted: m})
     conn.sendPresenceUpdate('composing', m.chat);
     const prompt = encodeURIComponent(text);
